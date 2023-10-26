fadfa9ae3ac10651fb17bae9b9e14d8b78a071b4
tuananh <nguyntanh2k5@gmail.com>
2023-10-26 16:59:44 +0700

    khoi tao du an

---
+++ BTTH1/hacker.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+	
+    char s[100];
+    scanf("%[^\n]%*c", &s);
+  	printf("hello, world!\n");
+    printf("%s",s);
+    s = ("life is beatutiful");
+    return 0;
+}
